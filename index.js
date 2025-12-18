28e76cdb9367cbf7c3c01c592cc7fa546026ff73
drakejohnson141 <ashishmantri1994@gmail.com>
2025-12-18 21:04:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import fs from "fs";
+import { sendToDiscord } from "./src/discord.js";
+import { getEpicFreeGames } from "./src/epic.js";
+import { getSteamDeals } from "./src/steam.js";
+import { getGogDeals } from "./src/gog.js";
+import { getLegacyGames } from "./src/legacy.js";
+
+const state = JSON.parse(fs.readFileSync("state.json"));
+
+async function run() {
+  const games = [
+    ...(await getEpicFreeGames()),
+    ...(await getSteamDeals()),
+    ...(await getGogDeals()),
+    ...getLegacyGames()
+  ];
+
+  for (const game of games) {
+    if (state.sent.includes(game.id)) continue;
+    await sendToDiscord(`🎮 **${game.title}**\n🔗 ${game.url}`);
+    state.sent.push(game.id);
+  }
+
+  fs.writeFileSync("state.json", JSON.stringify(state, null, 2));
+}
+run();
